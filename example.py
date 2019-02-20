058660c1773bdb58f4ec00a6e428252a9fb8b61a
maikeldotuk <maikel@maikel.uk>
2019-02-20 03:21:32 +0000

    Menu following the mouse, created

--- example.py
+++ example.py
@@ -1,9 +1,18 @@
 from PyQt5.QtWidgets import QApplication, QWidget, QMenu, qApp
 import sys
+import pyautogui
 
 
 def shit():
     print('I shit you not')
+
+
+def menu_is_hidden():
+    print('Menu has hidden')
+
+
+def get_mouse_position():
+    return pyautogui.position()
 
 
 def main():
@@ -11,11 +20,20 @@ def main():
     widget = QWidget()
     menu = QMenu(widget)
     menu.addAction('I shit you not')
-    menu.addAction('I shit you not')
     closeAction = menu.addAction('Close')
     closeAction.triggered.connect(qApp.quit)
+    menu.aboutToHide.connect(qApp.quit)
+    # How to show it where the mouse pointer is?
+    menu.setTitle('meeeeeh')
+    location = get_mouse_position()
+    menu.move(location.x, location.y)
+    menu.addSection('hello')
+    menu.addAction('I shit you not')
+    menu.addAction('I shit you not')
+    menu.addSeparator()
+    menu.addAction('I shit you not')
+    menu.addAction('I shit you not')
     menu.show()
-    menu.leaveEvent(shit)
     sys.exit(app.exec_())
 
 
