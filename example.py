8d5410fed36b563c9a70f55910f53d996ba6c824
maikeldotuk <maikel@maikel.uk>
2019-02-20 04:55:41 +0000

    Prepare it to be started with arguments

--- example.py
+++ example.py
@@ -1,4 +1,5 @@
 from PyQt5.QtWidgets import QApplication, QWidget, QMenu, qApp
+from PyQt5.QtGui import QIcon
 import sys
 import pyautogui
 
@@ -16,25 +17,33 @@ def get_mouse_position():
 
 
 def main():
-    app = QApplication([])
+    app = QApplication(sys.argv)
     widget = QWidget()
-    menu = QMenu(widget)
+    if 'show' in sys.argv:
+        show_menu(QMenu(widget))
+    if 'tray' in sys.argv:
+        print('tray version')
+        quit()
+    if len(sys.argv) == 1:
+        quit()
+    sys.exit(app.exec_())
+
+
+def show_menu(menu):
     menu.addAction('I shit you not')
     closeAction = menu.addAction('Close')
     closeAction.triggered.connect(qApp.quit)
     menu.aboutToHide.connect(qApp.quit)
-    # How to show it where the mouse pointer is?
-    menu.setTitle('meeeeeh')
-    location = get_mouse_position()
-    menu.move(location.x, location.y)
-    menu.addSection('hello')
     menu.addAction('I shit you not')
     menu.addAction('I shit you not')
     menu.addSeparator()
     menu.addAction('I shit you not')
     menu.addAction('I shit you not')
+    icon = QIcon('icon.ico')
+    menu.addAction(icon, 'Hoho')
+    location = get_mouse_position()
+    menu.move(location.x, location.y)
     menu.show()
-    sys.exit(app.exec_())
 
 
 if __name__ == '__main__':
